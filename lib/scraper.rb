9b3761a6e57b7b8058a5976aaa8f5e1c4930bb7b
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-04-16 20:58:14 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,9 @@
 require 'nokogiri'
-require 'httparty'
+require 'open-uri'
 
 url = "https://flatironschool.com/"
 
-html = HTTParty(url)
+html = open(url)
 
 doc = Nokogiri::HTML(html)
 
@@ -15,4 +15,32 @@ doc = Nokogiri::HTML(html)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+html = open("https://flatironschool.com/")
+
+Nokogiri::HTML(html)
+
+doc = Nokogiri::HTML(html)
+
+doc.css(".headline-26OIBN").text.strip 
+
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+
+courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+courses.each do |course|
+  puts course.text.strip
+end
 
